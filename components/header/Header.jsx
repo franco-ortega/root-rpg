f00b3662e172022b88fa5b45d86de622abac309b
Franco Ortega <francopdx@gmail.com>
2022-09-27 21:39:53 -0700

    create Header with image

---
+++ components/header/Header.jsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import RootArtwork from '../../public/images/root-capture.png';
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <div className={styles.Header}>
+      <Image src={RootArtwork} alt='' />
+      <nav>
+        <ul>
+          <li>Playbooks (classes)</li>
+          <li>Players Guide</li>
+          <li>Drives</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
