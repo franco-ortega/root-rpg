a26b1fc26a957149e1df1eb1181270ed815a9828
Franco Ortega <francopdx@gmail.com>
2022-10-08 10:18:02 -0700

    create CharacterList component with one Character

---
+++ components/characters/CharacterList.jsx
@@ -0,0 +1,12 @@
+import Character from './Character';
+
+const CharacterList = () => {
+  return (
+    <div>
+      <h1>Characters</h1>
+      <Character />
+    </div>
+  );
+};
+
+export default CharacterList;
