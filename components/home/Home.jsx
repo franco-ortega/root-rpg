9a1ee00f40cc1e18b47a54e1b4532ac958d18b83
Franco Ortega <francopdx@gmail.com>
2022-10-08 10:20:17 -0700

    add Home component with CharacterList

---
+++ components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CharacterList from '../characters/CharacterList';
+
+const Home = () => {
+  return (
+    <div>
+      <CharacterList />
+    </div>
+  );
+};
+
+export default Home;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
-export default function Home() {
-  return <div>Home</div>;
+import Home from '../components/home/Home';
+
+export default function HomePage() {
+  return <Home />;
 }
