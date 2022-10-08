6750172ac9bd3a4405758f765fec86cf1cde817f
Franco Ortega <francopdx@gmail.com>
2022-10-08 11:24:43 -0700

    add 5 more playbooks

--- components/characters/Character.jsx
+++ components/characters/Character.jsx
@@ -3,8 +3,8 @@ const Character = ({ character, drives, nature, connections }) => {
     <li>
       <h2>{character}</h2>
       <section>
-        Drives (pick 2): {drives[0].title}, {drives[1].title} {drives[2].title},{' '}
-        {drives[3].title}
+        Drives (pick 2): {drives[0].title}, {drives[1].title}, {drives[2].title}
+        , {drives[3].title}
       </section>
       <section>
         Nature (pick 1): {nature[0].title}, {nature[1].title}
