2f2bead9bc1582a23aea2509b8c98829f6293e43
Franco Ortega <francopdx@gmail.com>
2022-10-08 10:15:47 -0700

    create Character component with filler content

---
+++ components/characters/Character.jsx
@@ -0,0 +1,12 @@
+const Character = () => {
+  return (
+    <div>
+      <h2>Adventurer</h2>
+      <section>Drives (2): DriveOne, DriveTwo, DriveThree, DriveFour</section>
+      <section>Nature (1): NatureOne, NatureTwo</section>
+      <section>Connections (2): ConnectOne, ConnectTwo</section>
+    </div>
+  );
+};
+
+export default Character;
