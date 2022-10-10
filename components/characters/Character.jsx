b5ab75a8e102c15fb9294ab2db2d674399c44225
Franco Ortega <francopdx@gmail.com>
2022-10-09 21:43:59 -0700

    add grid to Character

--- components/characters/Character.jsx
+++ components/characters/Character.jsx
@@ -4,16 +4,17 @@ const Character = ({ character, drives, nature, connections }) => {
   return (
     <li className={styles.Character}>
       <h2>{character}</h2>
+      <hr />
       <ul>
         <li>
-          Drives (pick 2): {drives[0].title}, {drives[1].title}, {drives[2].title}
+          <span>Drives (pick 2)</span>: {drives[0].title}, {drives[1].title}, {drives[2].title}
           , {drives[3].title}
         </li>
         <li>
-          Nature (pick 1): {nature[0].title}, {nature[1].title}
+        <span>Nature (pick 1)</span>: {nature[0].title}, {nature[1].title}
         </li>
         <li>
-          Connections (both): {connections[0].title}, {connections[1].title}
+        <span>Connections (both)</span>: {connections[0].title}, {connections[1].title}
         </li>
 
       </ul>
