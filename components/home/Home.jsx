cc6bf3f9e143740bf1c8cd1e20fd5acfb74c2f71
Franco Ortega <francopdx@gmail.com>
2022-10-11 12:48:53 -0700

    add title font

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -1,5 +1,49 @@
+import TitleAndText from '../info/TitleAndText';
+import styles from './Home.module.scss';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={styles.Home}>
+      <h1>Root RPG</h1>
+      <p>These are some terms to help you get started.</p>
+      <ul>
+        <TitleAndText>
+          <span data-term>Drives</span> - These are the motivations for your
+          vagabond. Each vagabond selects 2 drives from their playbook. When you
+          complete a drive, you gain an advancement. You can advance with each
+          drive once per session. There may be sessions where none of your
+          drives are completed.
+        </TitleAndText>
+        <TitleAndText>
+          <span data-term>Nature</span> - This is the core of your being (for
+          better or worse). You can try to follow your nature or to avoid it,
+          but whenever you give in to your nature, you become reenergized.
+        </TitleAndText>
+        <TitleAndText>
+          <span data-term>Moves</span> - These are your actions. You make a move
+          any time there is reasonable uncertainty about the success of an
+          effort. For example, a bird can fly into a tree without making a move,
+          but if it is flying into a tree to escape danger or chase someone,
+          then a move is required.
+        </TitleAndText>
+        <TitleAndText>
+          <span data-term>Playbook</span> - This is your background / class /
+          species / relationships (friends, foes, factions, etc) all rolled into
+          a single character sheet.
+        </TitleAndText>
+        <TitleAndText>
+          <span data-term>Harm</span> - This is damage, and there are three (or
+          really four) kinds of damage. <span data-option>Depletion</span>{' '}
+          reduces access to the misc minor items that you usually have on hand
+          (rations, rope, torches, etc). <span data-option>Exhaustion</span>{' '}
+          reduces access to actions that take a notable amount of effort.{' '}
+          <span data-option>Injury</span> reduces access to being alive.{' '}
+          <span data-option>Wear</span> reduces access to major items (weapons,
+          armor, etc).
+        </TitleAndText>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
--- components/moves/Moves.jsx
+++ components/moves/Moves.jsx
@@ -1,10 +1,71 @@
+import TitleAndText from '../info/TitleAndText';
 import styles from './Moves.module.scss';
 
 const Moves = () => {
   return (
     <div className={styles.Moves}>
       <h1>Moves</h1>
-      <p>Coming soon.</p>
+      <p>
+        There are several categories of moves, and each category has a wide
+        variety of specific moves within it. Some moves will be determined by
+        the playbook, and other moves are chosen by the player. As you advance,
+        you will be able to access and/or select new moves.
+      </p>
+
+      <ul>
+        <TitleAndText>
+          <span data-term>Basic moves</span> cover most interations with most
+          people (NPCs and PCs), general items, and the immediate environment.
+          There are 8 basic moves that everyone can use:{' '}
+          <span data-option>attempt a roguish feat</span>,{' '}
+          <span data-option>figure someone out</span>,{' '}
+          <span data-option>persuade an NPC</span>,{' '}
+          <span data-option>read a tense situation</span>,{' '}
+          <span data-option>trick an NPC</span>,{' '}
+          <span data-option>trust fate</span>,{' '}
+          <span data-option>wreck something</span>,{' '}
+          <span data-option>help or interfere</span>, However, the{' '}
+          <span data-option>attempt a roguish feat</span> move includes a
+          selection of 9 options (<span data-option>Acrobatics</span>,{' '}
+          <span data-option>Blindside</span>,{' '}
+          <span data-option>Counterfeit</span>,{' '}
+          <span data-option>Disable Device</span>, <span data-option>Hide</span>
+          , <span data-option>Pick Locks</span>,{' '}
+          <span data-option>Pickpocket</span>,{' '}
+          <span data-option>Sleight of Hand</span>,{' '}
+          <span data-option>Sneak</span>), and each vagabond will start the game
+          with 1 - 4 of these options. Also, each playbook comes with set of 6
+          unique moves, and each vagabond will begin with 3 of these.
+        </TitleAndText>
+        <TitleAndText>
+          <span data-term>Weapon moves</span> are divided into{' '}
+          <span data-option>basic weapon moves</span> and{' '}
+          <span data-option>weapon skill moves</span>. All vagabonds have access
+          to all the <span data-option>basic weapon moves</span> (
+          <span data-option>engage in melee</span>,{' '}
+          <span data-option>grapple an enemy</span>,{' '}
+          <span data-option>target someone</span>). To use a particular{' '}
+          <span data-option>weapon skill move</span>, the vagabond must have the
+          skill, must have a weapon that can benefit from the skill, and must be
+          in a situation where the skill can be applied (i.e. you cannot disarm
+          someone who does not have a weapon). Each vagabond starts with one{' '}
+          <span data-option>weapon skill move</span> based on their playbook.
+        </TitleAndText>
+        <TitleAndText>
+          <span data-term>Reputation moves</span> can come into play when you
+          meet or interact with a notable faction member or woodland citizen.
+          Each vagabond will begin with a reputation score of 0 with all
+          factions, but they will also have at least two points of prestige
+          (positive) and one point of notoriety (negative) that can be applied
+          to any faction.
+        </TitleAndText>
+        <TitleAndText>
+          <span data-term>Travel moves</span>
+        </TitleAndText>
+        <TitleAndText>
+          <span data-term>Session moves</span>
+        </TitleAndText>
+      </ul>
     </div>
   );
 };
